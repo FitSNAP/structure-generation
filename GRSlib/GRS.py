82dc800db2b96a6ae1ba2234653578a8d98ec008
coreen-mullen <coreenmullen.8052@gmail.com>
2025-10-07 16:39:39 -0600

    adding ensemble score to GRS.py and scoring.py

--- GRSlib/GRS.py
+++ GRSlib/GRS.py
@@ -2,9 +2,8 @@ from GRSlib.parallel_tools import ParallelTools
 from GRSlib.io.input import Config
 from GRSlib.converters.convert_factory import convert
 from GRSlib.motion.scoring_factory import scoring
-from GRSlib.motion.scoring import Scoring
+from GRSlib.motion.scoring import Scoring, elems, get_desc_count
 from GRSlib.motion.motion import Gradient, Optimize, Create
-
 import random, copy, os, glob, shutil
 import numpy as np
 
@@ -165,6 +164,36 @@ class GRS:
             raise RuntimeError(">>> Found unmatched BASIS for target and current descriptors")
             
         return score
+    
+    def get_ensemble(self,data):
+        print("Get ensemble called.")
+        print("Using elems:", elems)
+        scores = None
+        try:
+            scores = self.score.ensemble_score(
+                n_totconfig=10,  # Example value
+                data_path='bcc.data',  # Example value
+                cross_weight=1.0,
+                self_weight=1.0,
+                randomize_comps=False,
+                mincellsize=54,
+                maxcellsize=55,
+                target_comps={'W': 1.0},  # Ensure this is a dictionary
+                min_typ_global='box',
+                soft_strength=0.0,
+                nelements=len(elems),  # Ensure elems is defined
+                n_descs=get_desc_count('coupling_coefficients.yace'),
+                mask=None,  # Set this as needed
+                rand_comp=1)
+            print("Scores returned from ensemble_score:", scores)
+        except Exception as e:
+            print(f"An error occured while calculating the ensemble score: {e}")
+            return None 
+        if scores is None or len(scores) == 0:
+            print("No scores were calculated returning None."
+            raise RuntimeError("Ensemble score could not be calculated.")
+
+        return scores 
 
     def propose_structure(self):
         """
